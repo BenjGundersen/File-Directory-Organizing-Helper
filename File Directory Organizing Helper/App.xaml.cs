2bec16ce3efde8f88ecda7313d30c1830b7c9095
Ben Gundersen <121984660+BenjGundersen@users.noreply.github.com>
2024-05-25 11:16:17 -0600

    Add project files.

---
+++ File Directory Organizing Helper/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace File_Directory_Organizing_Helper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ File Directory Organizing Helper/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.IO.Enumeration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace File_Directory_Organizing_Helper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void START_Click(object sender, RoutedEventArgs e)
+        {
+            ///File.Delete(FileName);
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog openFolderDialog1 = new OpenFolderDialog();
+            openFolderDialog1.Title = "Select";
+            openFolderDialog1.InitialDirectory = @"C:\";
+            openFolderDialog1.ShowDialog();
+        }
+
+        private void Frame_Navigated_1(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
